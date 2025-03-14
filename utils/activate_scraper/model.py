2a01f897843904c018141e5a067a743a85bd1eb9
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-03-15 00:27:46 +1100

    feat: pydantic model for event and calendar data

---
+++ utils/activate_scraper/model.py
@@ -0,0 +1,24 @@
+from datetime import date, datetime
+
+from pydantic import BaseModel
+
+
+class EventData(BaseModel):
+    start_time: datetime
+    end_time: datetime
+    date: date
+    cost: int
+    category: list[str]
+    summary: str
+    description: str
+    link: str
+    updated_at: datetime
+    location: str
+
+
+class CalendarData(BaseModel):
+    DTSTART: datetime
+    DTEND: datetime
+    SUMMARY: str
+    DESCRIPTION: str
+    LOCATION: str
