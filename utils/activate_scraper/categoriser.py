6431a44fb9bbd5171bbb4411b91e1eb24e73582e
withoutrhymeorreason <ghnsndrgml@gmail.com>
2025-03-15 20:41:13 +1100

    Added new unknown category and also changes program to work using static class instance so it can be used everywhere after initialisation in main

--- utils/activate_scraper/categoriser.py
+++ utils/activate_scraper/categoriser.py
@@ -10,15 +10,15 @@ class Categoriser:
         model = genai.GenerativeModel('gemini-1.5-pro')
         self.chat = model.start_chat(history=[])
 
-    def categorise(self, summary_text):
-        system_prompt = """You are an event categorizer. You will receive an event summary and must categorize it into one of these categories: {Sport, Cultural, Academic, Networking, Gaming, Health}. Respond with only the category name. The following is the event summary to categorize:\n"""
-        response = self.chat.send_message(str(system_prompt + summary_text), stream=False)
-        return response.text.strip()
+    def categorise(summary_text):
+        system_prompt = """You are an event categorizer. You will receive an event summary and must categorize it into any of these categories: {Unknown, Sport, Cultural, Academic, Networking, Gaming, Health}. An event can belong to multiple categories. If the summary given is empty or unable to be deciphered, you must categorise it as Unknown. If the category is Unknown, the event cannot belong to any other categories. Respond with only the categories, delimitted by a comma if it belongs to many. The following is the event summary to categorize:\n"""
+        response = Categoriser._instance_.chat.send_message(str(system_prompt + summary_text), stream=False)
+        return response.text.strip().split(", ")
   
 # FOR TEST PURPOSES
   
 # if __name__ == "__main__":
-#     categoriser = Categoriser()
+#     Categoriser._instance_ = Categoriser()
 #     summary_text = """Course Description:
 # The Kung Fu Club is hosting a 5 week introduction to Kung Fu course, teaching the fundamentals of kung fu, using Long Fist as a basis of the course.
 # Long Fist, also known as Changquan in Chinese, is a traditional style of Chinese martial arts (Kung Fu) characterized by its extended, fluid movements and emphasis on long-range techniques. It is one of the major forms of Chinese martial arts and is practiced both for self-defence and physical fitness.
@@ -40,4 +40,5 @@ class Categoriser:
 
 # The course will run each Monday 6:30pm-7:30pm from the 17th of February 2025 through to the 17th of March 2025 (inclusive). Additionally, from 7:30pm-8pm after each class, participants are welcome to stay for further training on more advanced exploration of techniques and applications.
 # """
-#     print(categoriser.categorise(summary_text))
+#     summary_text = "aesDGasrh"
+#     print(Categoriser.categorise(summary_text))
